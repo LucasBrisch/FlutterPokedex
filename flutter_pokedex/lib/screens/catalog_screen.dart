9e89070a8d61a9018fe1451a7cb4b94c28379235
Lucas Brisch <lucasbrisch@gmail.com>
2026-08-31 20:04:45 -0300

    adds API service and show data

--- flutter_pokedex/lib/screens/catalog_screen.dart
+++ flutter_pokedex/lib/screens/catalog_screen.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
-class CatalogScreen extends StatelessWidget {
+import '../models/pokemon.dart';
+import '../services/pokeapi_service.dart';
+
+class CatalogScreen extends StatefulWidget {
   const CatalogScreen({super.key});
+
+  @override
+  State<CatalogScreen> createState() => _CatalogScreenState();
+}
+
+class _CatalogScreenState extends State<CatalogScreen> {
+  final PokeApiService _pokeApiService = PokeApiService();
+
+  late Future<List<Pokemon>> _pokemonFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pokemonFuture = _pokeApiService.getPokemonList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +28,36 @@ class CatalogScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pokédex'),
       ),
-      body: const Center(
-        child: Text('Catálogo de Pokémon'),
+      body: FutureBuilder<List<Pokemon>>(
+        future: _pokemonFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(), //Indicador de carregamento RF09
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Não foi possível carregar os Pokémon.',
+              ),
+            );
+          }
+
+          final pokemonList = snapshot.data ?? [];
+
+          return ListView.builder(
+            itemCount: pokemonList.length,
+            itemBuilder: (context, index) {
+              final pokemon = pokemonList[index];
+
+              return ListTile(
+                title: Text(pokemon.name),
+              );
+            },
+          );
+        },
       ),
     );
   }
---
+++ flutter_pokedex/lib/services/pokeapi_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../models/pokemon.dart';
+
+class PokeApiService {
+  final String baseUrl = 'https://pokeapi.co/api/v2';
+
+  Future<List<Pokemon>> getPokemonList({
+    int limit = 20,
+    int offset = 0,
+  }) async {
+    final url = Uri.parse(
+      '$baseUrl/pokemon?limit=$limit&offset=$offset',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      final List results = data['results'];
+
+      return results
+          .map((pokemon) => Pokemon.fromJson(pokemon))
+          .toList();
+    }
+
+    throw Exception('Erro ao carregar os Pokémon');
+  }
+}
